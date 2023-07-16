d55789047213b5c6888053b489a44476a9af3911
Janny Nyendwa <44328865+nyendwa@users.noreply.github.com>
2023-07-16 02:27:47 +0200

    Title: Add calculator class library
    Body: This is the initial commit for the calculator class library. The library includes a set of mathematical functions that can be used to develop a simple calculator in Python, WPF, or WinForm. The library is designed to be easy to use and integrate into existing codebases. This is the first step towards creating a valuable tool for developers who need to perform mathematical calculations in their applications.

---
+++ CalculatorPy/CalculatorPy.cs
@@ -0,0 +1,43 @@
+﻿namespace CalculatorPy.DLL
+{
+    public class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return a / b;
+        }
+
+        public static double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public static double SquareRoot(double a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Cannot take square root of negative number.");
+            }
+            return Math.Sqrt(a);
+        }
+    }
+}
